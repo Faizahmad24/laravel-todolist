f21843d370bf0073215fbc347693a2263ee3997a
Ahmad Faiz <ahmadfaiz309@gmail.com>
2024-01-24 10:47:18 +0700

    initial commit

---
+++ tests/Feature/UserServiceTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use App\Services\UserService;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class UserServiceTest extends TestCase
+{
+    private UserService $userService;
+
+    protected function setUp():void
+    {
+        parent::setup();
+
+        $this->userService = $this->app->make(UserService::class);
+    }
+
+    public function testSample()
+    {
+        self::assertTrue(true);
+    }
+}
